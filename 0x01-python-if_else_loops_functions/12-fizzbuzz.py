d609690300ced9f6b9dd79fb851e6b4b3350dc48
OnyeEbube <okontaonyeka@gmail.com>
2022-09-21 07:09:26 +0000

    Updating 12-fizzbuzz.py file

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,10 +5,10 @@ def fizzbuzz():
             if i == 99:
                 print("FizzBuzz")
             else:
-                print("FizzBuzz", end=' ')
+                print("FizzBuzz", end='')
         elif i % 3 == 0:
-            print("Fizz", end=' ')
+            print("Fizz", end='')
         elif i % 5 == 0:
-            print("Buzz", end=' ')
+            print("Buzz", end='')
         else:
             print("{:d}".format(i))
