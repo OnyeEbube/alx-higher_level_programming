4bb8273e17b76c9376ed9b218977232045d178e4
OnyeEbube <okontaonyeka@gmail.com>
2022-09-22 14:05:55 -0700

    Updating 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,5 +3,6 @@ if __name__ == "__main__":
     from sys import argv
         add = 0
         for s in argv[1:]:
-            add += int(s)
-        print("{:d}".format(add))
+            if s != sys.argv[0]:
+                add += int(s)
+        print(add)
