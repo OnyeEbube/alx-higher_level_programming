567da9e483f46232bd2cbc3d64816c0c89a23a7d
OnyeEbube <okontaonyeka@gmail.com>
2022-09-22 14:08:29 -0700

    Updating 3-infinite_add.py file

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-        add = 0
-        for s in argv[1:]:
-            if s != sys.argv[0]:
-                add += int(s)
-        print(add)
+    import sys
+    add = 0
+    for s in argv[1:]:
+        if s != sys.argv[0]:
+            add += int(s)
+    print(add)
