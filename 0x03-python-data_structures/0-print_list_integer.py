cea5e3761c88b89a4ebf6fe30b2d9ec7454a89e0
OnyeEbube <okontaonyeka@gmail.com>
2022-09-23 10:10:10 -0700

    Updating 0-print_list_integer.py file

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    length = len(my_list) - 1
-    for l in length:
+    length = len(my_list)
+    for l in range(length):
         print("{}".format(my_list[l]))
