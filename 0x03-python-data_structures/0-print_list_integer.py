842de79a1539d29fb2070ebc36d447a96357c628
OnyeEbube <okontaonyeka@gmail.com>
2022-09-23 10:04:47 -0700

    Adding 0-print_list_integer.py file

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    length = len(my_list) - 1
+    for l in length:
+        print("{}".format(my_list[l]))
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,1 @@
+This module contains trainings on python data structures
