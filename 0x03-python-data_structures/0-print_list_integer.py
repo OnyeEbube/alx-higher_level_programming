c30bfa2f27bf169e637a2e5bf4dde6235bebd275
OnyeEbube <okontaonyeka@gmail.com>
2022-09-24 12:24:20 -0700

    Updating 2 files

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     length = len(my_list)
-    for l in range(length):
-        print("{:d}".format(my_list[l]))
+    for s in range(length):
+        print("{:d}".format(my_list[s]))
--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@ def element_at(my_list, idx):
     length = len(my_list)
     if idx > length or idx < 0:
         return(None)
-    for l in range(length):
-        if l == idx:
+    for s in range(length):
+        if s == idx:
             return(my_list[idx])
