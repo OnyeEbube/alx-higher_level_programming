85da5d106ba5e428f87406d234c631d2b3fc7ef0
OnyeEbube <okontaonyeka@gmail.com>
2022-09-26 05:00:05 -0700

    Adding 8-multiple_returns.py file

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,5 +2,7 @@
 def multiple_returns(sentence):
     length = len(sentence)
     firstChr = sentence[0]
+    if length == 0:
+        firstChr = None
     new_tuple = (length, firstChr)
     return(new_tuple)
