b59604f246d8a888ce37d5ffdd41391e83252925
OnyeEbube <okontaonyeka@gmail.com>
2022-10-11 09:54:46 -0700

    Adding 2-safe_print_list_integers.py file

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 9
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+        except (IndexError, ValueError, TypeError):
+            break
+        else:
+            count += 1
+    print()
+    return (count)
