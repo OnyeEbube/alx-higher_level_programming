2c7fc4f6fdb59ac7d46f9930a8d945a986be150f
OnyeEbube <okontaonyeka@gmail.com>
2022-10-13 08:42:40 -0700

    Updating 5-square.py file

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -23,10 +23,13 @@ class Square:
 
     def my_print(self):
         """Defining a method to print square."""
-        for i in range(self.__size):
-            for j in range(self.__size):
-                print("#", end='')
+        if self.__size == 0:
             print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end='')
+                print()
 
     @property
     def size(self):
