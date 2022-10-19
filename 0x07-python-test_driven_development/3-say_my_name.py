8968da910a7021bcf23632f44350d6c707afeccb
OnyeEbube <okontaonyeka@gmail.com>
2022-10-19 12:07:30 +0000

    Adding 4-print_square

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-
 """Defining a function."""
 
 
 def say_my_name(first_name, last_name=""):
-    """Creating a function that prints "My name is <first name> <last name>"."""
+    """Creating a function that prints
+    "My name is <first name> <last name>".
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
