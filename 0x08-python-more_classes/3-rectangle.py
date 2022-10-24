657fa3b4f3a1800e5dc8f924ce6b9ce4316e3023
OnyeEbube <okontaonyeka@gmail.com>
2022-10-24 18:07:36 +0000

    Updating 3-rectangle.py file

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -6,8 +6,9 @@
 class Rectangle:
     """This class defines a rectangle."""
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        """method that initialize the instance."""
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
