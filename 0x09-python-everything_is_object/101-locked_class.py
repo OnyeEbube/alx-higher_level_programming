25bb5900b871c4e9f14305479e13a51944173189
OnyeEbube <okontaonyeka@gmail.com>
2022-10-24 13:16:53 +0000

    Advanced tasks

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" This module contains a class that avoids dynamically
+created attributes """
+
+
+class LockedClass():
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """ init method"""
+        pass
