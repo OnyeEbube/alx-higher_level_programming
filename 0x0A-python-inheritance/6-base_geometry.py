16d8c474c87946e4e5305ad18a5f7b72b5821de4
OnyeEbube <okontaonyeka@gmail.com>
2022-10-31 15:02:24 +0000

    Updating 6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,10 +1,9 @@
-#1/usr/bin/python3
+#!/usr/bin/python3
 
 """ Creating a class. """
 
 
 class BaseGeometry:
     """This is a class that defines a method area."""
-
     def area(self):
         raise Exception("area() is not implemented")
