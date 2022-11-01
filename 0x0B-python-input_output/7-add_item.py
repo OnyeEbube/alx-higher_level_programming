7de49ebb09a37ceb244ca627e9cd3a7a1e1c71d5
OnyeEbube <okontaonyeka@gmail.com>
2022-11-01 17:29:31 +0000

    Adding 3 files

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,6 +8,7 @@ import sys
 import os.path
 
 
+""" Importing file."""
 save_file = __import__('5-save_to_json_file').save_to_json_file
 load_file = __import__('6-load_from_json_file').load_from_json_file
 
