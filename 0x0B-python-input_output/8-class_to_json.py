d098aa856ae0876ba7ac9107de751b3816c15d68
OnyeEbube <okontaonyeka@gmail.com>
2022-11-01 17:57:35 +0000

    Adding 8-class_to_json.py file

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" Creating a function. """
+
+
+def class_to_json(obj):
+    """ This function returns the dictionary description with simple data
+    structure for JSON serialization of an object.
+    """
+    res = {}
+    if hasatrr(obj, "__dict__"):
+        res = obj.__dict__.copy()
+    return res
