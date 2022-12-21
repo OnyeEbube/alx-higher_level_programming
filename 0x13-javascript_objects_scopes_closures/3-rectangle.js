78f2c6e88ae13a824660af41c27cffef273ffb47
OnyeEbube <okontaonyeka@gmail.com>
2022-12-21 13:16:01 +0000

    Adding 2 files

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,12 +1,22 @@
 #!/usr/bin/node
 /**
- * Defines a rectanglw with height and width
+ * Prints a rectangle
  */
 class Rectangle {
   constructor (w, h) {
     if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
       this.width = w;
       this.height = h;
+    }
+  }
+
+  print() {
+    for (let i = 0; i < height; i++) {
+      let myVar = '';
+      for (let j = 0; j < width; j++) {
+        myVar = myVar + 'X';
+      }
+      console.log(myVar);
     }
   }
 }
