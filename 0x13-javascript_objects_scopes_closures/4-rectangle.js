7b9cc13f6ed50875b5b3c439e974ad49bbd884ef
OnyeEbube <okontaonyeka@gmail.com>
2022-12-21 14:08:05 +0000

    Adding 4-rectangle.js file

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+/**
+ * Prints a rectangle
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let myVar = '';
+      for (let j = 0; j < this.width; j++) {
+        myVar = myVar + 'X';
+      }
+      console.log(myVar);
+    }
+  }
+
+  rotate () {
+    let x = this.width;
+    this.width = this.height;
+    this.height = x;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
