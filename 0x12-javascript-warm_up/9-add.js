c1d3e8b6d23079f884966bdbce8d912e77fb8f64
OnyeEbube <okontaonyeka@gmail.com>
2023-03-13 20:48:23 +0000

    File #9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,10 +4,11 @@
  * The second argument is the second integer
  * You have to define a function with this prototype: function add(a, b)
  */
-const a = parseInt(process.argv[2]);
-const b = parseInt(process.argv[3]);
-function add(a, b) {
-  let sum = a + b;
-  return (sum);
+const a = process.argv[2];
+const b = process.argv[3];
+
+function add (a, b) {
+  return (a + b);
 }
-console.log(add(a, b));
+
+console.log(add(parseInt(a), parseInt(b)));
