65d1da600973575126f4becf323ee613dc83de40
OnyeEbube <okontaonyeka@gmail.com>
2023-03-13 18:57:28 +0000

    File #5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+ * A script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer:
+ * If the argument can’t be converted to an integer, print “Not a number”
+ */
+const myVar = process.argv;
+if (myVar[2]) {
+	console.log('My number : ' + parseInt(myVar[2]));
+} else {
+	console.log('Not a number');
+}
