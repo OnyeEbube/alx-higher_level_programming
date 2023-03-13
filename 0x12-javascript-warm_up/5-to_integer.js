bbdbb360ff7f4b0ffd7ab8b1ab3bf9a7f9c6c432
OnyeEbube <okontaonyeka@gmail.com>
2023-03-13 19:32:27 +0000

    Updating File #6

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,9 +3,9 @@
  * A script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer:
  * If the argument can’t be converted to an integer, print “Not a number”
  */
-const myVar = process.argv;
+const myVar = process.argv
 if ((!myVar[2]) || (!parseInt(myVar[2]))) {
-  console.log('Not a number');
+  console.log('Not a number')
 } else {
-  console.log('My number: ' + parseInt(myVar[2]));
+  console.log('My number: ' + parseInt(myVar[2]))
 }
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -8,5 +8,5 @@
 const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i;
 for (i = 0; i < 3; i++) {
-	console.log(myVar[i]);
+  console.log(myVar[i]);
 }
