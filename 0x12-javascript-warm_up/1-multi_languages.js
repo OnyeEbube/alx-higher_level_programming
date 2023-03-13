1bfe3a9c41113de23bd20d5cc865b99f2c332ddd
OnyeEbube <okontaonyeka@gmail.com>
2023-03-13 16:23:23 +0000

    File #1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* A script that prints 3 lines:
+ * The first line: “C is fun”
+ * The second line: “Python is cool”
+ * The third line: “JavaScript is amazing”
+ */
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
