0f8958cf9374ba57b09a250c2348fae93e4e37cd
OnyeEbube <okontaonyeka@gmail.com>
2023-03-13 19:02:28 +0000

    Updating File #5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,7 +5,7 @@
  */
 const myVar = process.argv;
 if (myVar[2]) {
-	console.log('My number : ' + parseInt(myVar[2]));
+	console.log('My number: ' + parseInt(myVar[2]));
 } else {
 	console.log('Not a number');
 }
