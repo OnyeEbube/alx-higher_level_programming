cc1e15816369211df13c73c97fb06d406ac9382c
OnyeEbube <okontaonyeka@gmail.com>
2023-03-13 17:24:51 +0000

    Updating File #3

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -4,4 +4,4 @@
  * The second line: “Python is cool”
  * The third line: “JavaScript is amazing”
  */
-console.log('C is fun\nPython is cool\nJavaScript is amazing');
+console.log('C is fun\nPython is cool\nJavaScript is amazing')
