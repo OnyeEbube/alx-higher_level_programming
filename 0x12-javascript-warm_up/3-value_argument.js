5aba2ecbd0f9e9c77bd78885466748a888d6d309
OnyeEbube <okontaonyeka@gmail.com>
2023-03-13 18:18:45 +0000

    File #3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,9 +3,9 @@
  * A script that prints the first argument passed to it:
  * If no arguments are passed to the script, print “No argument”
  */
-const argc = process.argv
+const argc = process.argv;
 if (!argc[2]) {
-  console.log('No argument')
+  console.log('No argument');
 } else {
-  console.log(argc[2])
+  console.log(argc[2]);
 }
