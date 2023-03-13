5be4be4e51d3d61788de47154a0db7dcd407c74e
OnyeEbube <okontaonyeka@gmail.com>
2023-03-13 17:13:51 +0000

    File #2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/* A script that prints a message depending on the number of arguments passed:
+ * If no arguments are passed to the script, print “No argument
+ * If only one argument is passed to the script, print “Argument found"
+ * Otherwise, print “Arguments found”
+ */
+const argc = process.argv.length;
+if (argc == 3) {
+	console.log('Argument found');
+} else if (argc > 3) {
+	console.log('Arguments found');
+} else {
+	console.log('No argument');
+}
