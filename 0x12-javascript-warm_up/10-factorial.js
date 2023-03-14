ccde6402d782c3228b780aa2dc4fba74f5627eb9
OnyeEbube <okontaonyeka@gmail.com>
2023-03-14 14:09:44 +0000

    File #11

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -6,12 +6,12 @@
  * You must do it recursively
  * You must use a function
  */
-const a = parseInt(process.argv[2]);
+const a = parseInt(process.argv[2])
 function factorial (a) {
   if ((!a) || (a === 1)) {
-    return (1);
+    return (1)
   } else {
-    return ((a) * factorial(a - 1));
+    return ((a) * factorial(a - 1))
   }
 }
-console.log(factorial(a));
+console.log(factorial(a))
