c9e27cca1564b8250bc48d9c53d507f757cdd93a
OnyeEbube <okontaonyeka@gmail.com>
2023-03-14 14:29:43 +0000

    File #13

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/* A function that returns the addition of 2 integers
+ * The function must be visible from outside
+ * The name of the function must be add
+ */
+exports.add = (a, b) => a + b;
