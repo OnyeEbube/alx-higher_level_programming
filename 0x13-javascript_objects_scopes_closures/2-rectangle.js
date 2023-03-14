6670a378d20bca460605464485169d23e858ce2f
OnyeEbube <okontaonyeka@gmail.com>
2023-03-14 20:52:23 +0000

    File #3

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,9 +2,9 @@
 class Rectangle {
   constructor (w, h) {
     if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
-      this.width = w;
-      this.height = h;
+      this.width = w
+      this.height = h
     }
   }
 }
-module.exports = Rectangle;
+module.exports = Rectangle
--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,6 +6,7 @@ class Rectangle {
       this.height = h;
     }
   }
+
   print () {
     for (let x = 0; x < this.height; x++) {
       let i = '';
