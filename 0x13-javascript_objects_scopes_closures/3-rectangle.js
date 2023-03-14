eed2bfeb18cd6794394d2e720f93cb57e4fe8987
OnyeEbube <okontaonyeka@gmail.com>
2023-03-14 20:37:39 +0000

    File #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && 3 > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    for (let x = 0; x < this.height; x++) {
+      let i = '';
+      for (let y = 0; y < this.width; y++) {
+        i = i + 'X';
+      }
+        console.log(i);
+    }
+  }
+}
+module.exports = Rectangle;
