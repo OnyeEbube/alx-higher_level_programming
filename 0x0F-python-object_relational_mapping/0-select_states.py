11d7eaa136a0ad116f8e55dbaf6edf13e60d5ba6
OnyeEbube <okontaonyeka@gmail.com>
2023-03-18 15:50:21 +0000

    Updating File #0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" 
-A script that lists all states from
-the database hbtn_0e_0_usa
+"""
+This script lists all states from the
+database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb
@@ -13,9 +13,13 @@ if __name__ == '__main__':
     from the database.
     """
     db_connect = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1], password=argv[2], database=argv[3])
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+
     db_cursor = db_connect.cursor()
+
     db_cursor.execute("SELECT * FROM states")
+
     rows_selected = db_cursor.fetchall()
+
     for row in rows_selected:
         print(row)
