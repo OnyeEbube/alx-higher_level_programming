57a3d81f8355a2278935afcd5cfd909be4482876
OnyeEbube <okontaonyeka@gmail.com>
2023-04-25 15:07:14 +0000

    Adding file #4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+const starWars = process.argv[2];
+const basicId = '18';
+let count = 0;
+
+request.get(starWars, (error, response, body) => {
+  if (error) {
+    console,log(error);
+  } else {
+    const data = JSON.parse(body);
+    data.results.forEach((film) => {
+      film.characters.forEach((character) => {
+        if (character.includes(basicId)) {
+	  count += 1;
+	}
+      });
+    });
+    console.log(count);
+  }
+});
